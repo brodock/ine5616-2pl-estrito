74fb8b7ef3b3441872ea4d19964c52079e7e9680
LKRaider <LKRaider@9267a322-4751-0410-b906-4d4a8fbae30e>
2008-07-06 17:24:23 +0000

    novas classes

--- DM.py
+++ DM.py
@@ -1,14 +1,13 @@
-#!/usr/bin/env python
-
-class Database(object):
-
-    def __init__(self):
-        '''Initialize database'''
+class DataManager():
+	
+	def __init__(self):
+	    # initialize database
         self.database = {
             'A':1000,
             'B':2000,
             'C':3000,
-            'D':4000}
+            'D':4000,
+            'E':2500}
     
     # sgbd functions
     def read(self, key):
@@ -19,7 +18,7 @@ class Database(object):
         '''Write a register to database'''
         self.database[key] = value
     
-    def parse(self, commands):
+    def parse(commands):
         '''Parse all transaction commands'''
         for cmd in commands:
             exec(cmd)

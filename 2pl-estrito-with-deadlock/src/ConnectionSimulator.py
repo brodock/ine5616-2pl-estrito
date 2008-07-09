7abf4a3213707d72366ff7fa4f70dbe2fd92ee12
LKRaider <LKRaider@9267a322-4751-0410-b906-4d4a8fbae30e>
2008-07-09 19:12:17 +0000

    reworked all logging

--- 2pl-estrito-with-deadlock/src/ConnectionSimulator.py
+++ 2pl-estrito-with-deadlock/src/ConnectionSimulator.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-import time
 from TransactionManager import TransactionManager, Transaction
-from LockManager import LockManager
 from DataManager import DataManager
+from LockManager import LockManager
+from Logging import Log
+import time
 
-class ConnectionSimulator():
+
+class ConnectionSimulator(object):
     def __init__(self):
         self.transactions = []
         self.running = True
@@ -17,51 +19,50 @@ class ConnectionSimulator():
         ('read', 'Registro3', 'y'),
         ('write', 'Registro3', 'var.x + var.y')]
         self.transactions.append(transaction_cmd)
-        
+
         transaction_cmd = [
         ('read', 'Registro1', 'x'),
         ('read', 'Registro2', 'y'),
         ('write', 'Registro2', 'var.x + var.y')]
         self.transactions.append(transaction_cmd)
-        
+
         transaction_cmd = [
         ('read', 'Registro5', 'x'),
         ('read', 'Registro4', 'y'),
         ('write', 'Registro4', 'var.x'),
         ('write', 'Registro5', 'var.y')]
         self.transactions.append(transaction_cmd)
-        
+
         transaction_cmd = [
         ('read', 'Registro4', 'x'),
         ('read', 'Registro5', 'y'),
         ('write', 'Registro5', 'var.x + 99'),
         ('write', 'Registro4', 'var.y + 99')]
-        
+
         self.transactions.append(transaction_cmd)
         print "ConnectionSimulator loaded!"
-    
+
     def run(self):
         for t in self.transactions:
                 trans = Transaction(t)
                 TM.append(trans)
         while self.running:
             try:
-                if DM.data_changed:
-                    print "\nEstado atual dos dados:"
-                    for d in DM.database:
-                        print "%s = %s" % (d, DM.database[d])
-                    print ""
-                    DM.data_changed = False
                 time.sleep(1)
             except KeyboardInterrupt, ex:
+                TM.running = False
+                log.running = False
                 self.running = False
-            
-                
+
+
 if __name__ == '__main__':
     print "Starting demonstration aplication to strict 2pl with deadlock solving"
-    LM = LockManager()
-    DM = DataManager()
-    TM = TransactionManager(LM, DM)
-    TM.start()
+    log = Log()
+    DM = DataManager(log)
+    LM = LockManager(log)
+    TM = TransactionManager(LM, DM, log)
     CM = ConnectionSimulator()
+    log.DM = DM
+    log.start()
+    TM.start()
     CM.run()

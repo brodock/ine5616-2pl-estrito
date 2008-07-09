7bb5d64f1e84efb253850386a60bba292e8238b3
LKRaider <LKRaider@9267a322-4751-0410-b906-4d4a8fbae30e>
2008-07-09 04:29:01 +0000

    changes from gobby session

--- trunk/2pl-estrito-with-deadlock/src/LockManager.py
+++ trunk/2pl-estrito-with-deadlock/src/LockManager.py
@@ -4,72 +4,84 @@
 class LockManager(object):
     
     def __init__(self):
+        '''
+        The lock table supports the following format:
+        {reg: {Tx: {'S': n} } }
+        or
+        {reg: {Tx: {'W': 1} } }
+        
+        '''
         self.lock_table = {}
-     
-    def lock(self, tx_id, data_item, mode):
-        '''
-        Acquire a lock, returning whether succeeded or not
-        WARNING: the following method is a brainfuck
-        '''
-        if self.lock_table.has_key(data_item):
-            # our data item already has locks on it
-            if 'write' in self.list_modes(data_item):
-                # there is a write lock
-                if mode == 'write' and tx_id in self.lock_table[data_item]:
-                    # we have the write lock and are requesting again
-                    # this is ok
-                    return True
-                else:
-                    # we are either requesting a read on a write locked data item
-                    # or another transaction holds the write lock
-                    # this is not ok
-                    return False
-            else:
-                # there are only read locks
-                    if mode == 'write':
-                        if self.lock_table[data_item].has_key(tx_id):
-                            # we have a read lock and want to make it to a
-                            # write lock, so we are requesting a lock upgrade
-                            return self.upgradelock(tx_id, data_item)
-                        else:
-                            # cannot write lock because other transaction
-                            # hold the read locks
-                            return False
-                    else:
-                        # already have read lock or getting new one
-                        # either way, it works
-                        self.lock_table[data_item][tx_id] = mode
-                        return True
+        print "LockManager carregado com sucesso!"
+    
+    def shared_lock(self, tx_id, data_item):
+        '''Acquire shared lock (read) on a data item'''
+        lock_type = self.check_lock(data_item)
+        if lock_type == 'U':
+            self.shared_list(tx_id, data_item)
+        elif lock_type == 'S':
+            self.shared_list(tx_id, data_item)
         else:
-            # data item has no locks yet
-            # so we grant any lock
-            self.lock_table[data_item] = {tx_id: mode}
-            return True
-                
+            # lock_type == 'X'
+            return
+            wait() #TODO
+    
+    def exclusive_lock(self, tx_id, data_item):
+        '''Acquire exclusive lock (write) on a data item'''
+        lock_type = self.check_lock(data_item)
+        if lock_type == 'U':
+            self.exclusive_list(tx_id, data_item)
+        elif lock_type == 'S':
+            if self.lock_table[data_item].has_key(tx_id):
+                if len(self.lock_table[data_item]) == 1:
+                # only we have shared lock on it, so upgrade is possible
+                self.exclusive_list(tx_id, data_item)
+            else:
+                return
+                wait() #TODO
+        else:
+            # lock_type == 'X'
+            return
+            wait() #TODO
+    
+    def shared_list(self, tx_id, data_item):
+        '''Add transaction to shared lock list on data item'''
+        lock_list = self.lock_table[data_item]
+        if lock_list.has_key(tx_id):
+            lock_list[tx_id]['S'] += 1
+        else:
+            lock_list[tx_id] = {'S': 1}
+    
+    def exclusive_list(self, tx_id, data_item):
+        '''Add transaction to exclusive lock list on data item'''
+        self.lock_table[data_item][tx_id] = {'W': 1}
+    
+    def lock_type(self, data_item):
+        '''
+        Return lock type over a data item:
+        'U' for unlocked, 'S' for shared and 'X' for exclusive
+        
+        '''
+        if not self.lock_table.has_key(data_item):
+            return 'U'
+        elif 'S' in list_modes(data_item):
+            return 'S'
+        else:
+            return 'X'
+    
     def list_modes(data_item):
         '''List all lock modes for a data item'''
         locks = self.lock_table[data_item]
         modes = [locks[tx] for tx in locks]
         return modes
-        
-    def upgradelock(self, tx_id, data_item):
-        '''Upgrade a read lock to a write lock'''
-        if len(self.lock_table[data_item]) == 1:
-            # only we have lock on it, so upgrade is possible
-            self.lock_table[data_item][tx_id] == 'write'
-            return True
-        else:
-            # cannot upgrade since other transactions
-            # are locking same data item
-            return False
-            
+    
     def unlock(self, tx_id, data_item):
         '''Remove a transaction lock from a data item'''
         print "Removendo lock da %s " % tx_id
         del(self.lock_table[data_item][tx_id])
         
     def unlock_all(self, tx_id):
-        '''Remove all lock held by a transaction'''
+        '''Remove all locks held by a transaction'''
         for data_item, txs in self.lock_table.items():
             if tx_id in txs:
                 self.unlock(tx_id, data_item)
